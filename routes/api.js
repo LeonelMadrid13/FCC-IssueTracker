b91d03b4018d17f28eeaa512814147b6e310409d
Leonel Madrid <68981970+LeonelMadrid13@users.noreply.github.com>
2023-11-24 00:38:08 +0000

    finished

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,95 @@
-'use strict';
+"use strict";
+
+const { uniqueID } = require("mocha/lib/utils");
+let projectDB = {};
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
+      let querys = req.query
+      let response = projectDB[project];
+      if(!querys){
+        return res.json(response);
+      }
+      if(querys.open){
+        response = response.filter(issue => issue.open == querys.open )
+      }
+      if(querys.created_by){
+          response = response.filter(issue => issue.created_by == querys.created_by )
+      }
+      if(querys.assigned_to){
+          response = response.filter(issue => issue.assigned_to == querys.assigned_to )
+      }
+      if(querys.status_text){
+          response = response.filter(issue => issue.status_text == querys.status_text )
+      }
+      if(querys._id){
+          response = response.filter(issue => issue._id == querys._id )
+      }
+      return res.json(response) 
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      if (!projectDB[project]) {
+        projectDB[project] = [];
+      }
+      if (
+        !req.body.created_by ||
+        !req.body.issue_title ||
+        !req.body.issue_text
+      ) {
+        return res.status(400).json({ error: "required field(s) missing" });
+      }
+
+      let date = new Date();
+      let _id = uniqueID();
+      let {issue_title, issue_text, created_by} = req.body
+      let newIssue = {
+        _id,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to: req.body.assigned_to || "",
+        status_text: req.body.status_text || "",
+        created_on: date,
+        updated_on: date,
+        open: true,
+      };
+      projectDB[project] = [...projectDB[project], newIssue];
+      return res.json(newIssue);
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
+      let {_id, ...fields} = req.body
+
+      if(!_id) return res.status(400).json({ error: 'missing _id' })
+      if(Object.keys(fields).length === 0) return res.status(400).json({ error: 'no update field(s) sent', _id })
       
+      let updatedIssue = projectDB[project].filter(issue => issue._id == _id )
+      if(updatedIssue.length === 0) return res.status(400).json({ error: 'could not update', '_id': _id })
+      
+      Object.keys(fields).forEach(key => {
+        updatedIssue[0][key] = fields[key]
+        updatedIssue[0].updated_on = new Date()
+      })
+      
+      return res.json({  result: 'successfully updated', _id })
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
+      let {_id} = req.body
+      if(!_id) return res.status(400).json({ error: 'missing _id' })
+
+      let delIssue = projectDB[project].filter(issue => issue._id == _id )
+      const index = projectDB[project].indexOf(delIssue[0]);
+      if(delIssue.length === 0) return res.status(400).json({ error: 'could not delete', '_id': _id })
+      projectDB[project].splice(index, 1)
+      return res.json({ result: 'successfully deleted', '_id': _id })
     });
-    
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,250 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const assert = chai.assert;
-const server = require('../server');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
+const expect = chai.expect;
+
+const server = require("../server");
+const { createMochaInstanceAlreadyDisposedError } = require("mocha/lib/errors");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+const ENDPOINT = "/api/issues/test";
+
+suite("Functional Tests", function () {
+  	let firstInsertedID;
+  	suite("POST to /api/issues/{project}", ( ) => {
+      	test("Issue with every field", (done) => {
+			chai
+			.request(server)
+			.post(ENDPOINT)
+			.send({
+				issue_title: "Faux Issue Title",
+				issue_text: "Functional Test - Required Fields Only",
+				created_by: "fCC",
+				assigned_to: "Me",
+				status_text: "QA",
+			})
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.have.property("_id");
+				expect(res.body)
+				.to.have.property("issue_title")
+				.to.equal("Faux Issue Title");
+				expect(res.body)
+				.to.have.property("issue_text")
+				.to.equal("Functional Test - Required Fields Only");
+				expect(res.body).to.have.property("created_by").to.equal("fCC");
+				expect(res.body).to.have.property("assigned_to").to.equal("Me");
+				expect(res.body).to.have.property("status_text").to.equal("QA");
+				expect(res.body)
+				.to.have.property("open")
+				.to.be.a("boolean")
+				.and.to.equal(true);
+
+				firstInsertedID = res.body._id;
+				done();
+			});
+		});
+		test("Issue with only required fields", (done) =>{
+			chai
+			.request(server)
+			.post(ENDPOINT)
+			.send({
+				issue_title: "Faux Issue Title",
+				issue_text: "Functional Test - Required Fields Only",
+				created_by: "Leonel",
+			})
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.have.property("_id");
+				expect(res.body)
+					.to.have.property("issue_title")
+					.to.equal("Faux Issue Title");
+				expect(res.body)
+					.to.have.property("issue_text")
+					.to.equal("Functional Test - Required Fields Only");
+				expect(res.body).to.have.property("created_by").to.equal("Leonel");
+				expect(res.body).to.have.property("assigned_to").to.equal("");
+				expect(res.body).to.have.property("status_text").to.equal("");
+				expect(res.body)
+					.to.have.property("open")
+					.to.be.a("boolean")
+					.and.to.equal(true);
+
+				firstInsertedID = res.body._id;
+				done();
+			});
+		})
+		test("Issue with missing required fields", (done) =>{
+			chai
+			.request(server)
+			.post(ENDPOINT)
+			.send({
+				issue_title: "Faux Issue Title",
+				issue_text: "Functional Test - Required Fields Only",
+			})
+			.end((err, res) => {
+				expect(res.status).to.equal(400);
+				expect(res.body).to.have.property('error').to.be.equal('required field(s) missing');
+				done();
+			});
+		})
+    });
+	suite("GET from /api/issues/{project}", ( ) => {
+		test("Issues on a project", ( done ) => {
+			chai
+			.request(server)
+			.get(ENDPOINT)
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).is.a('array');
+				expect(res.body[0]).to.have.property('_id')
+				expect( res.body[0] ).to.have.property( 'issue_title' );
+				expect( res.body[0] ).to.have.property( 'issue_text'  );
+				expect( res.body[0] ).to.have.property( 'assigned_to' );
+				expect( res.body[0] ).to.have.property( 'status_text' );
+				expect( res.body[0] ).to.have.property( 'created_by'  );
+				expect( res.body[0] ).to.have.property( 'created_on'  );
+				expect( res.body[0] ).to.have.property( 'updated_on'  );
+				expect( res.body[0] ).to.have.property( 'open'        );
+				done( );
+			})
+		})
+		test("Issues on a project with one filter", (done) => {
+			chai
+			.request(server)
+			.get(ENDPOINT)
+			.query({created_by: 'Leonel'})
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).is.a('array');
+				expect(res.body[0]).to.have.property('_id')
+				expect( res.body[0] ).to.have.property( 'issue_title' );
+				expect( res.body[0] ).to.have.property( 'issue_text'  );
+				expect( res.body[0] ).to.have.property( 'assigned_to' );
+				expect( res.body[0] ).to.have.property( 'status_text' );
+				expect( res.body[0] ).to.have.property( 'created_by'  ).to.equal('Leonel');
+				expect( res.body[0] ).to.have.property( 'created_on'  );
+				expect( res.body[0] ).to.have.property( 'updated_on'  );
+				expect( res.body[0] ).to.have.property( 'open'        );
+				done( );
+			})
+		})
+		test("Issues on a project with multiple filters", (done) => {
+			chai
+			.request(server)
+			.get(ENDPOINT)
+			.query({created_by: 'fCC', assigned_to: 'Me'})
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).is.a('array');
+				expect(res.body[0]).to.have.property('_id')
+				expect( res.body[0] ).to.have.property( 'issue_title' );
+				expect( res.body[0] ).to.have.property( 'issue_text'  );
+				expect( res.body[0] ).to.have.property( 'assigned_to' ).to.equal('Me');
+				expect( res.body[0] ).to.have.property( 'status_text' );
+				expect( res.body[0] ).to.have.property( 'created_by'  ).to.equal('fCC');
+				expect( res.body[0] ).to.have.property( 'created_on'  );
+				expect( res.body[0] ).to.have.property( 'updated_on'  );
+				expect( res.body[0] ).to.have.property( 'open'        );
+				done( );
+			})
+		})
+	})
+	suite("PUT to /api/issues/{project}", () => {
+		test("Update one field on an issue", (done) => {
+			chai
+			.request(server)
+			.put(ENDPOINT)
+			.send({_id: firstInsertedID, issue_title: "Updated Title"})
+			.end((err, res) => {
+				expect(res.status).to.equal(200)
+				expect(res.body).to.have.property('_id').to.equal(firstInsertedID)
+				expect(res.body).to.have.property('result').to.equal('successfully updated')
+				done()	
+			})
+		})
+		test("Update multiple fields on an issue", (done) => {
+			chai
+			.request(server)
+			.put(ENDPOINT)
+			.send({_id: firstInsertedID, issue_title: "Updated Title", issue_text:"Updated Text"})
+			.end((err, res) => {
+				expect(res.body).to.have.property('_id').to.equal(firstInsertedID)
+				expect(res.body).to.have.property('result').to.equal('successfully updated')
+				done()
+			})
+		})
+		test("Update an issue with missing _id", (done) => {
+			chai
+			.request(server)
+			.put(ENDPOINT)
+			.send({ issue_title: "Updated Title"})
+			.end((err, res) => {
+				expect(res.status).to.equal(400)
+				expect(res.body).to.have.property('error').to.equal('missing _id')
+				done()
+			})
+		})
+		test("Update an issue with no fields to update", (done) => {
+			chai
+			.request(server)
+			.put(ENDPOINT)
+			.send({ _id: firstInsertedID })
+			.end((err, res) => {
+				expect(res.status).to.equal(400)
+				expect(res.body).to.have.property('error').to.equal('no update field(s) sent')
+				expect(res.body).to.have.property('_id').to.equal(firstInsertedID)
+				done()
+			})
+		})
+		test("Update an issue with an invalid _id", (done) => {
+			chai
+			.request(server)
+			.put(ENDPOINT)
+			.send({ _id: 'firstInsertedID', issue_title: "Updated Title" })
+			.end((err, res) => {
+				expect(res.status).to.equal(400)
+				expect(res.body).to.have.property('error').to.equal('could not update')
+				expect(res.body).to.have.property('_id').to.equal('firstInsertedID')
+				done()
+			})
+		})
+	})
+	suite(`DELETE from ${ENDPOINT}`, () =>{
+		test("Delete an issue", (done)=>{
+			chai
+			.request(server)
+			.delete(ENDPOINT)
+			.send({_id: firstInsertedID})
+			.end((err, res) => {
+				expect(res.status).to.equal(200)
+				expect(res.body).to.have.property('result').to.equal('successfully deleted')
+				expect(res.body).to.have.property('_id').to.equal(firstInsertedID)
+				done()
+			})
+		})
+		test("Delete an issue with an invalid _id", (done) => {
+			chai
+			.request(server)
+			.delete(ENDPOINT)
+			.send({_id: 'firstInsertedID'})
+			.end((err, res) => {
+				expect(res.status).to.equal(400)
+				expect(res.body).to.have.property('error').to.equal('could not delete')
+				expect(res.body).to.have.property('_id').to.equal('firstInsertedID')
+				done()
+			})
+		})
+		test("Delete an issue with missing _id", (done) => {
+			chai
+			.request(server)
+			.delete(ENDPOINT)
+			.send({})
+			.end((err, res) => {
+				expect(res.status).to.equal(400)
+				expect(res.body).to.have.property('error').to.equal('missing _id')
+				done()
+			})
+		})
+	})
 });
